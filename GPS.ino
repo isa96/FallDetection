daa7bd5b8a8a6317163f70a2f5d931fdaa66f519
isa96 <izzan.abad@gmail.com>
2020-05-27 02:16:27 +0700

    FallDetection

---
+++ GPS.ino
@@ -0,0 +1,15 @@
+void displayInfo()
+{
+//  if (gps.location.isValid())
+//  {
+//    Serial.print("Latitude: ");
+//    Serial.println(gps.location.lat(), 6);
+//    Serial.print("Longitude: ");
+//    Serial.println(gps.location.lng(), 6);
+//  }
+//  else
+//  {
+//    Serial.println("Location: Not Available");
+//  }
+//  delay(1000);
+}
